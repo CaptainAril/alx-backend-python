c0850a1d30f379de1b67082145e80d6c6bedabe9
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-03 08:45:32 +0100

    concats string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines a type-annotated function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenation of the two string parameters."""
+    return str1 + str2
