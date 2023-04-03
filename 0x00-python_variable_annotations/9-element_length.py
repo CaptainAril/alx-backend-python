383c8cc0a22cd7b29c517a9f7bc389ddf851b4cf
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-03 13:39:22 +0100

    duct type

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Defines a type-annotated function.
 """
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of the lenght of each `lst` item."""
     return [(i, len(i)) for i in lst]
