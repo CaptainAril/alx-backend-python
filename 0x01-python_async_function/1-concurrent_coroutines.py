ac2d2dba1829d2f70a251c6a4efd84ac38a56094
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-04 16:34:18 +0100

    multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Creating coroutines."""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
 import asyncio
 from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     result = []
@@ -19,5 +20,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
                 temp = result[i]
                 result[i] = result[j]
                 result[j] = temp
-                
+
     return result
