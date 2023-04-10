9fecfc485c772f5798c92f86246d3c1650aee5a1
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-10 11:31:27 +0100

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """multiplies `lst` content."""
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
